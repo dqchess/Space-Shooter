086303867a581aa04e90cfc32c988681c3d5a766
Haripriya Aithal <haripriyaaithal@gmail.com>
2019-04-21 21:34:37 +0530

    Added background music

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour {
+    private void Awake() {
+        if (FindObjectsOfType(GetType()).Length > 1) {
+            Destroy(gameObject);
+        } else {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
