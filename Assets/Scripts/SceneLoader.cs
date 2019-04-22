1a2552e0db661134cbd7bf899dd93b088332cf8b
Haripriya Aithal <haripriyaaithal@gmail.com>
2019-04-22 14:54:43 +0530

    Added Loading screen

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -2,8 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class SceneLoader : MonoBehaviour {
+
+    [SerializeField] Slider loading;
+    [SerializeField] Text percentage;
+
+    public void LoadGamePlay() {
+        StartCoroutine(LoadAsynchronously());
+    }
+
+    IEnumerator LoadAsynchronously() {
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Game Play");
+        
+        while (!operation.isDone) {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            loading.value = progress;
+            percentage.text = (progress * 100).ToString("F0") + "%";
+            yield return null;
+        }
+    }
 
     public void LoadScene(string scene) {
         SceneManager.LoadScene(scene);
